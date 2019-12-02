463ff2bc92914f322b615266c02c97afee624a93
dgathogo <edgetech26@gmail.com>
2019-12-02 15:18:14 +0100

    create posts

---
+++ app/src/main/java/us/ait/postsphere/ForumActivity.kt
@@ -0,0 +1,80 @@
+package us.ait.postsphere
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.*
+import kotlinx.android.synthetic.main.activity_forum.*
+import us.ait.postsphere.adapter.PostAdapter
+import us.ait.postsphere.data.Post
+
+class ForumActivity : AppCompatActivity() {
+    private lateinit var postsAdapter: PostAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_forum)
+        setSupportActionBar(toolbar)
+        fab.setOnClickListener { view ->
+            startActivity(Intent(this@ForumActivity, CreatePostActivity::class.java))
+        }
+
+        postsAdapter = PostAdapter(this, FirebaseAuth.getInstance().currentUser!!.uid)
+
+        var linLayoutManager = LinearLayoutManager(this)
+        linLayoutManager.reverseLayout = true
+        linLayoutManager.stackFromEnd = true
+
+        recyclerPosts.layoutManager = linLayoutManager
+
+        recyclerPosts.adapter = postsAdapter
+
+        queryPosts()
+    }
+
+
+    fun queryPosts() {
+        val db = FirebaseFirestore.getInstance()
+        val query = db.collection("posts")
+
+        var allPostsListener = query.addSnapshotListener(
+            object : EventListener<QuerySnapshot> {
+                override fun onEvent(
+                    querySnapshot: QuerySnapshot?,
+                    e: FirebaseFirestoreException?
+                ) {
+
+                    if (e != null) {
+                        Toast.makeText(
+                            this@ForumActivity,
+                            "listen error: ${e.message}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                        return
+                    }
+
+                    for (dc in querySnapshot!!.getDocumentChanges()) {
+                        when (dc.type) {
+                            DocumentChange.Type.ADDED -> {
+                                val post = dc.document.toObject(Post::class.java)
+                                postsAdapter.addPost(post, dc.document.id)
+                            }
+                            DocumentChange.Type.MODIFIED -> {
+                                Toast.makeText(
+                                    this@ForumActivity,
+                                    "update: ${dc.document.id}",
+                                    Toast.LENGTH_LONG
+                                ).show()
+                            }
+                            DocumentChange.Type.REMOVED -> {
+                                postsAdapter.removePostByKey(dc.document.id)
+                            }
+                        }
+                    }
+                }
+            })
+    }
+}
--- app/src/main/java/us/ait/postsphere/data/Post.kt
+++ app/src/main/java/us/ait/postsphere/data/Post.kt
@@ -2,13 +2,16 @@ package us.ait.postsphere.data
 
 data class Post(
     var postId: String = "",
+    var postAuthor: String = "",
     var postTitle: String = "",
     var postBody: String = "",
+    var imgUrl: String = "",
     var postComments: MutableList<Comment>
 )
 
 data class Comment(
     var commentId: String = "",
+    var commentAuthour: String = "",
     var commentBody: String = "",
     var comments: MutableList<Comment>
 )
